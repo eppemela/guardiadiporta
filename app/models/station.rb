eb327d250a170f58bb5bcacf2941d9f1cb191bf4
eppemela <eppedema@gmail.com>
2015-03-24 13:19:59 +0100

    Refactoring function for counting total rx and tx transmitted bytes

--- app/models/station.rb
+++ app/models/station.rb
@@ -4,29 +4,12 @@ class Station < ActiveRecord::Base
     find_by_mac_addr(mac)
   end
 
+
   def self.find_or_create(mac_address, rx_counter, tx_counter, last_time_seen)
-
-    #TODO: refactor in a proper single function
-    
-    old_rx_value = get_last_rx_bytes(mac_address)
-    if ( (rx_counter - old_rx_value) > 0 )
-      diff_rx_value = old_rx_value + (rx_counter - old_rx_value)
-    else
-      diff_rx_value = old_rx_value + rx_counter
-    end
-
-    old_rx_value = get_last_rx_bytes(mac_address)
-    if ( (tx_counter - old_rx_value) > 0 )
-      diff_tx_value = old_rx_value + (tx_counter - old_rx_value)
-    else
-      diff_tx_value = old_rx_value + tx_counter
-    end
-
-
     attributes = {
       mac_addr: mac_address,
-      rx_bytes: diff_rx_value,
-      tx_bytes: diff_tx_value,
+      rx_bytes: get_total_tx_or_rx_bytes(mac_address, "rx", rx_counter),
+      tx_bytes: get_total_tx_or_rx_bytes(mac_address, "tx", tx_counter),
       last_seen: last_time_seen
     }
 
@@ -37,14 +20,33 @@ class Station < ActiveRecord::Base
     end
   end
 
+
+  def self.get_total_tx_or_rx_bytes(mac, type, new_value)
+    case type
+    when "rx"
+      old_value = get_last_rx_bytes(mac)
+    when "tx"
+      old_value = get_last_tx_bytes(mac)
+    end
+
+    if (new_value - old_value > 0)
+      diff_value = old_value + (new_value - old_value)
+    else
+      diff_value = old_value + new_value
+    end
+  end
+
+
   def self.get_last_rx_bytes(mac)
     s = get(mac)
     s.rx_bytes
   end
 
+
   def self.get_last_tx_bytes(mac)
     s = get(mac)
     s.tx_bytes
   end
+
 
 end
