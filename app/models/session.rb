0c9e94c7d07b14a9e291d9a898339e456644dfff
eppemela <eppedema@gmail.com>
2015-04-13 17:59:16 +0200

    Set up models

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,2 +1,24 @@
 class Session < ActiveRecord::Base
+  belongs_to :station
+
+  scope :open, -> { where(open: true) }
+  scope :closed, -> { where(open: false) }
+
+  def self.get(station_id)
+    where(station_id: station_id, open: true)
+  end
+
+
+  def self.find_or_create(station_id, start_time)
+    attributes_for_creation = {
+      station_id: station_id,
+      start: start_time,
+      open: true
+    }
+
+    if (get(station_id).empty?)
+      create(attributes_for_creation)
+    end
+  end
+
 end
