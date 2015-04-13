dcdd2e51b7edac800c3ded26db97a2aef7b059e8
eppemela <eppedema@gmail.com>
2015-04-13 19:02:09 +0200

    Add static controller for root index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :sessions
   resources :stations
+  root  "static#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
