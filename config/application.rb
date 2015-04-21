81f5c086985455583d40eb3c2ba80625ca8dc832
eppemela <eppedema@gmail.com>
2015-04-21 16:24:57 +0200

    Precompiled asset pipeline to serve in production

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module GuardiaDiPorta
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.web_console.whitelisted_ips = '192.168.2.0/16'
+    #config.web_console.whitelisted_ips = '192.168.2.0/16'
   end
 end
