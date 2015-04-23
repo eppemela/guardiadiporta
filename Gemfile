70b715bea0f8f651c1c1162d86c47ea5e0de9ee2
eppemela <eppedema@gmail.com>
2015-04-23 12:43:38 +0200

    Add Capistrano for deployment on remote Raspberry PI server

--- Gemfile
+++ Gemfile
@@ -57,6 +57,9 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 
+  # Use Capistrano for deployment
+  gem 'capistrano'
+
   # Access an IRB console on exception pages or by using <%= console %> in views
   #gem 'web-console', '2.0.0.beta3'
 
---
+++ config/deploy.rb
@@ -0,0 +1,65 @@
+set :application, "Guardia di Porta"
+set :repository,  "git@github.com:eppemela/guardiadiporta.git"
+set :deploy_to, "/home/pi/guardiadiporta/public"
+set :scm, :git
+set :branch, "master"
+set :user, "pi"
+set :group, "deployers"
+set :use_sudo, false
+set :rails_env, "production"
+set :deploy_via, :copy
+set :ssh_options, { :forward_agent => true }
+set :keep_releases, 5
+default_run_options[:pty] = true
+server "192.168.2.20", :app, :web, :db, :primary => true
+
+#set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+# role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+# role :app, "your app-server here"                          # This may be the same as your `Web` server
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+
+  desc "Symlink shared config files"
+  task :symlink_config_files do
+    run "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
+  end
+
+  # NOTE: I don't use this anymore, but this is how I used to do it.
+  # desc "Precompile assets after deploy"
+  # task :precompile_assets do
+  #   run <<-CMD
+  #     cd #{ current_path } &&
+  #     #{ sudo } bundle exec rake assets:precompile RAILS_ENV=#{ rails_env }
+  #   CMD
+  # end
+
+  desc "Restart applicaiton"
+  task :restart do
+    run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
+  end
+end
+
+after "deploy", "deploy:symlink_config_files"
+after "deploy", "deploy:restart"
+after "deploy", "deploy:cleanup"
