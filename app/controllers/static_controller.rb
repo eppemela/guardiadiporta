7659e619727d0e3f5b65e76b2cee2e85165e6772
eppemela <eppedema@gmail.com>
2015-05-15 20:07:08 +0200

    Add liteicons and info on daily visits and firs-in\last-out in Dashboard

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,19 @@ class StaticController < ApplicationController
 
   def index
     @present_stations = Station.today_visits
+    first_in_hash = {
+      name:  Session.today.first.station.name,
+      arrival_time:  Session.today.first.start
+    }
+    @first_in = first_in_hash
+
+    last_out_hash = {
+      name: Session.closed.today.last.station.name,
+      leaving_time: Session.closed.today.last.end
+    }
+    @last_out = last_out_hash
+    @total_visits = Session.today.group(:station_id)
+
   end
 
   def today
