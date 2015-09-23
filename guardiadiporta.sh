9603440b19385641d924bc7085e95b3be220292c
eppemela <eppedema@gmail.com>
2015-09-23 20:13:52 +0200

    Revert back to full path on the shell script

--- guardiadiporta.sh
+++ guardiadiporta.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "Getting connected clients list on $(date)"
-deploy_dir=`pwd`
 
-/usr/bin/ssh root@192.168.2.10 wl assoclist > $deploy_dir/stations_dump
+/usr/bin/ssh root@192.168.2.10 wl assoclist > /home/pi/guardiadiporta/current/stations_dump
